d5c4b3795453f54c41354315362f0f4ab4245174
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-03 14:08:53 +0100

    Pass deltaTime in seconds

--- src/sketch.js
+++ src/sketch.js
@@ -1,1 +1,1 @@
-function preload(){MainManager.preload()}function setup(){MainManager.setup()}function draw(){MainManager.draw(deltaTime)}
+function preload() { MainManager.preload() } function setup() { MainManager.setup() } function draw() { MainManager.draw(deltaTime / 1000.0) }
