a458da0e09c2dd5becad500a0d4bdc73b9115419
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-17 09:15:56 +0100

    Update domManager.js

--- src/managers/domManager.js
+++ src/managers/domManager.js
@@ -1,11 +1,5 @@
 const DOMManager = (function () {
   return {
-    startColorPicker: 0, endColorPicker: 0, spaceSelect: 0,
-
-    colorCount: 0, generatePaletteButton: 0,
-
-    domHeight: 0,
-
     preload() {
 
     },
